5020981e1a6d15021fd269b2dd32e16dc54ae73e
lukaszbialobrzeski <lbialobrzeski1@gmail.com>
2022-11-26 23:12:55 +0100

    Add reading input from user

--- src/main/java/pl/lukaszbialobrzeski/Main.java
+++ src/main/java/pl/lukaszbialobrzeski/Main.java
@@ -1,7 +1,19 @@
 package pl.lukaszbialobrzeski;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String userName = null;
+        System.out.println("Username?");
+        userName = sc.nextLine();
+
+        System.out.println(
+                "Use lower difficulty? (Y/N) [Leave empty for default of True]");
+        String diffChoice = "";
+        diffChoice = sc.nextLine();
+        sc.close();
+        boolean useLowerDiff = diffChoice.toLowerCase().equals("n");
     }
 }
